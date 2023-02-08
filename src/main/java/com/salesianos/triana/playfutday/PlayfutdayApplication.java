5c275911b2aa537f0f1a411d6e1c9300add9d3ef
Maylor <maylorbustamante2001@gmail.com>
2023-02-08 12:47:41 +0100

    Se ha creado el proyecto base

---
+++ src/main/java/com/salesianos/triana/playfutday/PlayfutdayApplication.java
@@ -0,0 +1,13 @@
+package com.salesianos.triana.playfutday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayfutdayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayfutdayApplication.class, args);
+	}
+
+}
